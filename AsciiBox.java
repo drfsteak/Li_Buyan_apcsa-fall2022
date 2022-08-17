537061ff973944b2da8ad0e12bafef1283588c7a
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-08-17 11:37:57 -0700

    Add files via upload

---
+++ AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - David Li
+//Date - 8/17/2022
+//Class - Period 3 CSA
+//Lab  - 0a 1
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	    System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
---
+++ Variables.java
@@ -0,0 +1,60 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		
+
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = 127        \t*");
+		System.out.println("*16 bit - shortOne = -32123   \t*");
+		System.out.println("*32 bit - intOne = 90877      \t*");
+		System.out.println("*64 bit - longOne = 999999999 \t*");
+		System.out.println("*                               *");
+		System.out.println("*      other integer types      *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = true              *");
+		System.out.println("*stringOne = hello world        *");
+		System.out.println("/////////////////////////////////");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
