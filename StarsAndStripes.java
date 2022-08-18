ea81f0e18f10a37154fb70d2b16c9dc2c3c8050d
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-08-18 11:17:59 -0700

    Add files via upload

---
+++ StarsAndStripes.java
@@ -0,0 +1,42 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   out.println();
+   }
+   
+   public void printA()
+   {	
+	   out.println("AAAAAAAAAAAAAAAAAA");
+   }
+ 
+   public void printABigBox()
+   { 	
+   }   
+}
---
+++ StarsRunner.java
@@ -0,0 +1,36 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes text = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      text.printTwentyStars();
+      text.printTwentyDashes();
+      text.printTwoBlankLines();
+      text.printTwentyStars();
+      text.printA();
+      text.printTwentyStars();
+      text.printTwentyDashes();
+      text.printTwentyStars();
+      text.printTwentyDashes();
+      text.printTwentyStars();
+      text.printTwentyDashes();
+      text.printTwoBlankLines();
+      text.printTwentyStars();
+      text.printA();
+      text.printTwoBlankLines();
+      text.printTwentyStars();
+      text.printA();
+   }
+}
