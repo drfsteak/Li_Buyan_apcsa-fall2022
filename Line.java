f543c06417b7adae2b8594d15fd7b37d0b914f6c
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-08-19 10:35:31 -0700

    Add files via upload

---
+++ Line.java
@@ -0,0 +1,13 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( double x1, int y1, int x2, int y2 )
+	{
+		return (y2 - y1)/(x2 - x1);
+	}
+
+}
