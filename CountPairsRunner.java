fc4e57640932392a21eff6f631564e1a54f8eebb
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-09-02 10:51:19 -0700

    Add files via upload

---
+++ CountPairsRunner.java
@@ -0,0 +1,15 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+import static java.lang.System.*; 
+
+public class CountPairsRunner
+{
+	public static void main( String[] args )
+	{
+		System.out.println( CountPairs.pairCounter("ddogccatppig") );
+		//add in all of the provided test cases from the lab handout	
+		
+	}
+}
---
+++ TriangleOneRunner.java
@@ -0,0 +1,16 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name
+
+import static java.lang.System.*;
+
+public class TriangleOneRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases		
+		TriangleOne test = new TriangleOne();
+		test.setWord("hippo");
+		test.print();
+	}
+}
