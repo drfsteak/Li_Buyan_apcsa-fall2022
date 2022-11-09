3723a7c86eebcf5973b897b9c4f25139e724cb91
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-11-08 17:30:46 -0800

    Update SpeedUpBall.java

--- Unit15/SpeedUpBall.java
+++ Unit15/SpeedUpBall.java
@@ -6,59 +6,51 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-class SpeedUpBall extends Ball
+public class SpeedUpBall extends Ball
 {
-
-   //instance variables
 
    public SpeedUpBall()
    {
-
-
+	   super();
    }
 
    public SpeedUpBall(int x, int y)
    {
-
-
+	   super(x, y);
    }
 
 
    public SpeedUpBall(int x, int y, int xSpd, int ySpd)
    {
-
-
+	   super(x, y, xSpd, ySpd);
    }
 
    public SpeedUpBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
    {
-
-
+	   super(x, y, wid, ht, xSpd, ySpd);
    }
 
 
    public SpeedUpBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
    {
-
-
-
-
+	   super(x, y, wid, ht, col, xSpd, ySpd);
    }
 
-   public void setXSpeed( int xSpd )
+   public void setXSpeed(int xSpd)
    {
-
-
-
-
+	   int adder = 1;
+	   if (xSpd < 0) {
+		   adder = -1;
+	   }
+	   super.setXSpeed(xSpd + adder);
    }
 
-   public void setYSpeed( int ySpd )
+   public void setYSpeed(int ySpd)
    {
-
-
-
-
+	   int adder = 1;
+	   if (ySpd < 0) {
+		   adder = -1;
+	   }
+	   super.setYSpeed(ySpd + adder);
    }
 }
-
