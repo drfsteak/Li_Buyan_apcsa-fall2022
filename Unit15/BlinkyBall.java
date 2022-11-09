f4b3da9660f4c24c081b588bd52fc2aaf6178953
drfsteak <111383736+drfsteak@users.noreply.github.com>
2022-11-08 17:31:07 -0800

    Update BlinkyBall.java

--- Unit15/BlinkyBall.java
+++ Unit15/BlinkyBall.java
@@ -7,57 +7,38 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.util.Random;
 
-class BlinkyBall extends Ball
-{
+public class BlinkyBall extends Ball {
 
-   //constructors
-   public BlinkyBall()
-   {
+	// constructors
+	public BlinkyBall() {
 		super();
-   }
+	}
 
-   public BlinkyBall(int x, int y)
-   {
+	public BlinkyBall(int x, int y) {
+		super(x, y);
+	}
 
+	public BlinkyBall(int x, int y, int wid, int ht) {
+		super(x, y, wid, ht);
+	}
 
+	public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd) {
+		super(x, y, wid, ht, xSpd, ySpd);
+	}
 
-   }
+	public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd) {
+		super(x, y, wid, ht, col, xSpd, ySpd);
+	}
 
-   public BlinkyBall(int x, int y, int wid, int ht)
-   {
+	public Color randomColor() {
+		int r = (int) (256 * Math.random());
+		int g = (int) (256 * Math.random());
+		int b = (int) (256 * Math.random());
+		return new Color(r, g, b);
+	}
 
-
-
-   }
-
-   public BlinkyBall(int x, int y, int wid, int ht, int xSpd, int ySpd)
-   {
-
-
-
-   }
-
-   public BlinkyBall(int x, int y, int wid, int ht, Color col, int xSpd, int ySpd)
-   {
-
-
-
-
-   }
-
-   public Color randomColor()
-   {
-   	int r = 0;		//use Math.random()
- 		int g = 0;
- 		int b = 0;
- 		return new Color(r,g,b);
-   }
-
-   public void moveAndDraw(Graphics window)
-   {
-
-
-
-
-   }
+	public void moveAndDraw(Graphics window) {
+		setColor(randomColor());
+		super.moveAndDraw(window);
+	}
 }
